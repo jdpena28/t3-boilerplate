8103b87b904f0c766f2ddc4c705aceca40f8a92a
jdpena28 <johnhenrich1128@gmail.com>
2022-11-26 12:08:35 +0800

    Add next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo title="T3 Boilerplate" description="T3 Boilerplate" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
